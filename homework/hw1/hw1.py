2bc49c532f5b1af0121cc8d12fc8adf8bd01f389
CoderKotori <947069773@qq.com>
2017-05-15 19:38:40 +0800

    something wrong

--- homework/hw1/hw1.py
+++ homework/hw1/hw1.py
@@ -3,18 +3,34 @@ import numpy as np
 
 pigeon = cv2.imread("../../testdata/hw1/pigeon.jpg")
 name = cv2.imread("../../testdata/hw1/name.jpg", cv2.CV_LOAD_IMAGE_GRAYSCALE)
-print name.shape
-print name.dtype
-result = pigeon.copy()
 
+# blue = np.zeros((pigeon.shape[0], pigeon.shape[1]), dtype=pigeon.dtype)
+# green = np.zeros((pigeon.shape[0], pigeon.shape[1]), dtype=pigeon.dtype)
+# red = np.zeros((pigeon.shape[0], pigeon.shape[1]), dtype=pigeon.dtype)
+#
+# blue[:, :] = pigeon[:, :, 0]
+# green[:, :] = pigeon[:, :, 1]
+# red[:, :] = pigeon[:, :, 2]
+blue, green, red = cv2.split(pigeon)
 
-print result.shape
-grey = cv2.split(name)[0]
-m,n = grey.shape
-print m, n
-count = 0
+m, n = name.shape
+addMatrix = np.zeros(name.shape, dtype=name.dtype)
 for i in range(m):
     for j in range(n):
-        if grey[i, j] not in [0, 255]:
-            count += 1
-print count
+        if name[i, j] < 127:
+            addMatrix[i, j] = 1
+        else:
+            addMatrix[i, j] = 0
+
+m, n = blue.shape
+for i in range(m):
+    for j in range(n):
+        if blue[i, j] % 2 != 0:
+            blue[i, j] -= 1
+tmp = blue + addMatrix
+print tmp
+result = cv2.merge([tmp, green, red])
+cv2.imwrite("../../outputdata/hw1/result.jpg", result)
+print result
+hidden = cv2.imread("../../outputdata/hw1/result.jpg")
+print hidden
---
+++ homework/hw1/restore.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+hidden = cv2.imread("../../outputdata/hw1/result.jpg")
+
+hiddenLayer = cv2.split(hidden)[0]
+print hidden
+# hiddenName = np.zeros(hiddenLayer.shape, dtype=hiddenLayer.dtype)
+# m, n = hiddenLayer.shape
+# for i in range(m):
+#     for j in range(n):
+#         hiddenName[i, j] = hiddenLayer[i, j] % 2
+#         if hiddenName[i, j] == 0:
+#             hiddenName[i, j] = 255
+#         else:
+#             hiddenName[i, j] = 0
+#
+# print hiddenName
+# cv2.imwrite("../../outputdata/hw1/test.jpg", hiddenName)
+# cv2.imshow("Name", hiddenName)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
