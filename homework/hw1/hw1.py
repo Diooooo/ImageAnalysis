9448a3822450a0187e004a697377a7a9e120d213
CoderKotori <947069773@qq.com>
2017-05-15 11:01:48 +0800

    some tests

--- homework/hw1/hw1.py
+++ homework/hw1/hw1.py
@@ -1,2 +1,20 @@
 import cv2
+import numpy as np
 
+pigeon = cv2.imread("../../testdata/hw1/pigeon.jpg")
+name = cv2.imread("../../testdata/hw1/name.jpg", cv2.CV_LOAD_IMAGE_GRAYSCALE)
+print name.shape
+print name.dtype
+result = pigeon.copy()
+
+
+print result.shape
+grey = cv2.split(name)[0]
+m,n = grey.shape
+print m, n
+count = 0
+for i in range(m):
+    for j in range(n):
+        if grey[i, j] not in [0, 255]:
+            count += 1
+print count
