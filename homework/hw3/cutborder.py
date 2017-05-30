de4b6415a3a450fe7676c2633d2fc99b33bf61c0
CoderKotori <947069773@qq.com>
2017-05-30 12:48:55 +0800

    homework3 done

---
+++ homework/hw3/cutborder.py
@@ -0,0 +1,79 @@
+#   rmBlackBorder: remove the black borders of one image
+#   return: cropped image
+def rmBlackBorder(
+        src,  # input image
+        thres,  # threshold for cropping: sum([r,g,b] - [0,0,0](black))
+        diff,  # max tolerable difference between black borders on two side
+        shrink  # number of pixels to shrink after the blackBorders removed
+):
+    #
+    #   remove the black border on both right and left side
+    #
+    nRow = src.shape[0]
+    nCol = src.shape[1]
+    left = -1
+    right = nCol
+
+    for i in [0, nRow / 2, nRow - 1]:
+        curLeft = -1
+        curRight = nCol
+        for j in range(0, nCol - 1):
+            if (sum(list(src[i, j])) <= thres and curLeft == j - 1):
+                curLeft += 1
+        if left == -1:
+            left = curLeft
+        if curLeft < left:
+            left = curLeft
+
+        for j in range(nCol - 1, 0, -1):
+            if (sum(list(src[i, j])) <= thres and curRight == j + 1):
+                curRight -= 1
+        if right == nCol:
+            right = curRight
+        if curRight > right:
+            right = curRight
+
+    if min(left, right) >= 1 and abs((left + 1) - (nCol - right)) <= diff and right - left > 0:
+        print 'left margin: %d\n' % left
+        print 'right margin: %d\n' % right
+
+        src = src[0: nRow - 1, left + 1 + shrink: right - 1 - shrink, :]
+    else:
+        src = src
+
+        #
+    #   remove the black border on both up and down side
+    #
+    nRow = src.shape[0]
+    nCol = src.shape[1]
+    up = -1
+    down = nRow
+
+    for j in [0, nCol / 2, nCol - 1]:
+        curUp = -1
+        curDown = nRow
+        for i in range(0, nRow - 1):
+            if (sum(list(src[i, j])) <= thres and curUp == i - 1):
+                curUp += 1
+        if up == -1:
+            up = curUp
+        if curUp < up:
+            up = curUp
+
+        for i in range(nRow - 1, 0, -1):
+            if (sum(list(src[i, j])) <= thres and curDown == i + 1):
+                curDown -= 1
+        if down == nRow:
+            down = curDown
+        if curDown > down:
+            down = curDown
+        down -= 15
+    if min(up, down) >= 1 and abs((up + 1) - (nRow - down)) <= diff and down - up > 0:
+        print 'up margin: %d\n' % up
+        print 'down margin: %d\n' % down
+
+        dst = src[up + 1 + shrink: down - 1 - shrink, 0: nCol - 1, :]
+    else:
+        dst = src
+
+    return dst
--- homework/hw3/test3.py
+++ homework/hw3/test3.py
@@ -1,13 +1,150 @@
+# -*- coding: UTF-8 -*-
 import numpy as np
 import cv2
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
+import cutborder
 
-img = cv2.imread('../../testdata/hw3/hw3.bmp')
-edges = cv2.Canny(img, 10, 20)
-cv2.imshow("hw3", edges)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+class Rectification:
+    def __init__(self, img_path):
+        self.img_path = img_path
+        self.rotate_path = '../../outputdata/hw3/rotate.bmp'
+        self.output_path = '../../outputdata/hw3/result.bmp'
+
+    def rotate(self):
+        """
+        对初始图像进行矫正，并存储矫正结果
+        :return: null
+        """
+        img = cv2.imread(self.img_path)                 # 读取图片
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    # 将RGB图像转换成灰度图
+        gray = cv2.GaussianBlur(gray, (5, 5), 15)       # 对灰度图进行高斯模糊处理
+        edged = cv2.Canny(gray, 240, 250)               # 使用Canny算法进行边缘检测
+
+        lines = cv2.HoughLines(edged, 1, np.pi / 180, 118)  # 使用霍夫变换检测边界直线
+        result = img.copy()
+        result[...] = 0
+        print len(lines[0])
+
+        contours = []
+        for i in range(len(lines[0])):
+            '''
+            求出边界直线交点作为待矫正图像顶点
+            '''
+            line1 = lines[0][i]
+            r1 = line1[0]
+            theta1 = line1[1]
+            for j in range(i + 1, len(lines[0])):
+                line2 = lines[0][j]
+                r2 = line2[0]
+                theta2 = line2[1]
+                if theta2 == theta1:
+                    continue
+                else:
+                    x0 = (r2 * np.sin(theta1) - r1 * np.sin(theta2)) / np.sin(theta1 - theta2)
+                    y0 = r1 / np.sin(theta1) - (np.cos(theta1) / np.sin(theta1)) * x0
+                    contours.append([y0, x0])
+        print contours
+        # exit()
+        # for line in lines[0]:
+        #     rho = line[0]  # 第一个元素是距离rho
+        #     theta = line[1]  # 第二个元素是角度theta
+        #     print "rho", rho
+        #     print "theta", theta
+        #     if (theta < (np.pi / 4.)) or (theta > (3. * np.pi / 4.0)):  # 垂直直线
+        #         # 该直线与第一行的交点
+        #         pt1 = (int(rho / np.cos(theta)), 0)
+        #         # 该直线与最后一行的焦点
+        #         pt2 = (int((rho - result.shape[0] * np.sin(theta)) / np.cos(theta)), result.shape[0])
+        #         # 绘制一条白线
+        #         cv2.line(result, pt1, pt2, 255)
+        #     else:  # 水平直线
+        #         # 该直线与第一列的交点
+        #         pt1 = (0, int(rho / np.sin(theta)))
+        #         # 该直线与最后一列的交点
+        #         pt2 = (result.shape[1], int((rho - result.shape[1] * np.cos(theta)) / np.sin(theta)))
+        #         # 绘制一条直线
+        #         cv2.line(result, pt1, pt2, 255, 1)
+
+        # for point in contours:
+        #     print point
+        #     cv2.circle(img, (point[1], point[0]), 5, (0, 0, 255), -1)
+        #
+        # cv2.imshow("img", img)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        # exit()
+        # contours.sort()
+
+        '''
+        对顶点进行排序以对应原图像顶点位置
+        '''
+        middle_x = sum(x[0] for x in contours) / len(contours)
+        middle_y = sum(y[1] for y in contours) / len(contours)
+        print middle_x, middle_y
+        tmp = contours[:]
+        print tmp
+        for point in tmp:
+            if point[0] < middle_x and point[1] < middle_y:
+                contours[0] = point
+            elif point[0] > middle_x and point[1] < middle_y:
+                contours[1] = point
+            elif point[0] > middle_x and point[1] > middle_y:
+                contours[2] = point
+            else:
+                contours[3] = point
+
+        src = np.float32(contours)
+        des = np.float32([[0, 0], [img.shape[0], 0], [img.shape[0], img.shape[1]], [0, img.shape[1]]])
+
+        print src
+        print des
+        # exit()
+        M = cv2.getPerspectiveTransform(des, src)           # 生成透射矩阵
+        print M
+        rotate = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))    # 对原图像进行透射变换
+        rotate = cutborder.rmBlackBorder(rotate, 20, 100, 2)
+        cv2.imwrite(self.rotate_path, rotate)      # 去除图像黑边
+
+    def markFlow(self):
+        """
+        标记图像内缺陷位置，并存储处理结果
+        :return: null
+        """
+        origin = cv2.imread(self.img_path)
+        rotate = cv2.imread(self.rotate_path)
+        gray = cv2.cvtColor(rotate, cv2.COLOR_BGR2GRAY)
+        result = rotate.copy()
+        m, n, _ = result.shape
+        threshold = 60          # 阈值
+        conn_threshold = 100    # 连通域阈值
+        '''
+            检测阈值并寻找连通域，染色为红
+        '''
+        for i in range(m):
+            for j in range(n):
+                if gray[i][j] < threshold:
+                    result[i][j] = [255, 0, 0]
+                    if i - 1 > 0 and gray[i - 1][j] < conn_threshold:
+                        result[i - 1][j] = [255, 0, 0]
+                        gray[i - 1][j] = threshold - 1
+                    if i + 1 < m and gray[i + 1][j] < conn_threshold:
+                        result[i + 1][j] = [255, 0, 0]
+                        gray[i + 1][j] = threshold - 1
+                    if j - 1 > 0 and gray[i][j - 1] < conn_threshold:
+                        result[i][j - 1] = [255, 0, 0]
+                        gray[i][j - 1] = threshold - 1
+                    if j + 1 > n and gray[i][j + 1] < conn_threshold:
+                        result[i][j + 1] = [255, 0, 0]
+                        gray[i][j + 1] = threshold - 1
+        cv2.imwrite(self.output_path, result)
+        plt.subplot(211)
+        plt.imshow(origin)
+        plt.subplot(212)
+        plt.imshow(result)
+        plt.show()
 
 
 if __name__ == '__main__':
-    pass
+    rectify = Rectification('../../testdata/hw3/hw3.bmp')
+    rectify.rotate()
+    rectify.markFlow()
