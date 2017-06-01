adce86739f80d17c311f064cdc2b88ef11ba6454
CoderKotori <947069773@qq.com>
2017-06-01 13:08:23 +0800

    add annotations

--- homework/hw1/test1.py
+++ homework/hw1/test1.py
@@ -11,6 +11,11 @@ class MsgHide:
         self.save_path = '../../outputdata/hw1/result.bmp'
 
     def hide(self):
+        """
+        将要隐藏信息的图片的red层的像素值偶数化，之后读取对应位置上需隐藏图片的像素值，若判定为黑色，则在
+        原图片对应位置red层像素+1
+        :return: 保存处理后的图片
+        """
         img = cv2.imread(self.image_path)
         msg = cv2.imread(self.hidden_msg)
         width, height = img.shape[:2]
@@ -25,6 +30,11 @@ class MsgHide:
         cv2.imwrite(self.save_path, img)
 
     def decipher(self):
+        """
+        创建一个zeros矩阵，大小与处理过后的图片一致
+        读取处理过后的图片red层的最后一位数值，输出到zeros矩阵中，若不为0则对应位置赋值白色像素值
+        :return: 使用matplotlib.pyplot进行结果展示
+        """
         origin = cv2.imread(self.image_path)
         img = cv2.imread(self.save_path)
         out = np.zeros(img.shape, img.dtype)
